34bd7a7bdd3be366222a3d6cdd292fb436cb038d
Ethan Willers <willers.enw@gmail.com>
2023-02-08 19:19:22 -0800

    use routes uncommented

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
