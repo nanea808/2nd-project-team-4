5b2718d54057a18d675b9cf9695a1d78cd93c771
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-02-09 14:57:34 -0800

    seeds now working

--- controllers/api/list-routes.js
+++ controllers/api/list-routes.js
@@ -1,3 +1,33 @@
 const router = require('express').Router();
+const {User, List, Item, Group, GroupList} = require('../../models');
+
+//get on all lists.
+router.get('/', async (req, res) => {
+    try {
+        const listData = await List.findAll({
+            //include: the user the item belongs to, the items in the list, and all groups with the list.
+            include: [{model: User}, {model: Item}, {model: Group, through: GroupList}, ]
+        });
+        res.status(200).json(listData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req,res) => {
+    try {
+        const listData = await List.findByPk(req.params.id, {
+            include: [{model: User},{model: Item}, {model: Group, through: GroupList}]
+        });
+
+        if(!listData) {
+            res.status(404).json({message: 'No list with that ID.'});
+            return;
+        }
+        res.status(200).json(listData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
