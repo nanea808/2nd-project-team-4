e9abdb293592b73bce96ce481e25005bc3566e75
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-02-08 19:16:47 -0800

    get route for user

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const groupRoutes = require('./group-routes');
+const itemRoutes = require('./item-routes');
+const listRoutes = require('./list-routes');
+const userRoutes = require('./user-routes');
+
+router.use('/groups', groupRoutes);
+router.use('/users', itemRoutes);
+router.use('/lists', listRoutes);
+router.use('/users', userRoutes);
+
+module.exports = router;
