b9c01700da5a1f8141e2cdc34efc174a64e66a0e
Ethan Willers <willers.enw@gmail.com>
2023-02-09 19:26:56 -0800

    hadlebars engine and homepage view setup

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const routes = require('./controllers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({});
+
+// Set view engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
