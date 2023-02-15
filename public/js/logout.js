e303ba0ce87f90687ceb370736c1d53a37f1d997
Ethan Willers <willers.enw@gmail.com>
2023-02-15 01:28:35 -0800

    added username, password hiding, and jquery script to signup form

---
+++ public/js/logout.js
@@ -0,0 +1,16 @@
+$(() => {
+  const logout = async () => {
+    const response = await fetch("/api/users/logout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location = "/";
+    } else {
+      alert("Failed to log out.");
+    }
+  };
+
+  $("#logout").click(logout);
+});
