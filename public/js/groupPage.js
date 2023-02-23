5c7c9c236502407ff7d52c91d1d0461aea5c666b
Ethan Willers <willers.enw@gmail.com>
2023-02-22 17:49:06 -0800

    lists display on user click

--- public/js/groupPage.js
+++ public/js/groupPage.js
@@ -1,10 +1,20 @@
 $(() => {
-    const lists = $('#lists').children();
-
+    const showList = (event) => {
+        const lists = $('#lists').children();
+        const element = event.target;
+        if (element.matches("li")) {
+            // console.log(element.dataset.user);
+            for (const list of lists) {
+                // console.log(list);
+                if (list.dataset.owner === element.dataset.user) {
+                    list.style.display = "inline";
+                } else {
+                    list.style.display = "none";
+                }
+            }
+        }
+    } 
     
 
-    
-    $('.list-group').children().click(() => {
-        
-    });
+    $('.list-group').children().click(showList);
 });
