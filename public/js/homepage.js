d050ab99c392332cf28b602c5717e0d227e47e47
Sarah <sarah.k.hopper@gmail.com>
2023-02-26 20:30:24 -0800

    add code to homepage.js to attempt new group create from homepage

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -27,10 +27,36 @@ $(() => {
     };
 
     // create new group
+    // const { Group, User, GroupUser, GroupList, List } = require('../../models');
+    // const router = require('express').Router();
+
     // function createGroup() {
-    //     const newGroupTitle = $('#group-title').val().trim();
-    //     const inviteeName = $('#groupUser-name').val().trim();
-    //     const inviteeEmail = $('#groupUser-email').val().trim();
+    //     const group_title = $('#group-title').val().trim();
+    //     const groupUser = $('#groupUser-name').val().trim();
+    //     // const email = $('#groupUser-email').val().trim(); // users need to be registered and logged in to look at their groups, but we don't currently have functionality to send email invites to users to join group and/or register if they don't already have an account. 
+
+    //     router.post('/', async (req, res) => {
+    //         const response = await Group.create(req.body, {
+    //             where: {
+    //                 owning_user_id: req.session.userID,
+    //                 title: group_title,
+    //             },
+    //             include: [
+    //                 { model: GroupUser, where: { user_id: groupUser } }, // how do we make sure group_id gets assigned to groupuser? where: group_id: this.req.id or something along those lines?
+    //                 // { model: GroupList, through: { model: Group, where: { } } },
+    //             ],
+    //             // headers: {
+    //             //     'Content-Type': 'application/json'
+    //             // }
+    //         });
+
+    //         if (response.ok) {
+    //             document.location.reload('/');
+    //         } else {
+    //             alert('Could not create new group.');
+    //             console.log('Error: couldn not create new group.');
+    //         }
+    //     });
     // };
 
     // create new list
