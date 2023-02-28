1e569de5cb2db0c26cbf20ed4803bc2420894bde
Sarah <sarah.k.hopper@gmail.com>
2023-02-27 16:54:55 -0800

    add name attributes to list form inputs for parsing, add code to attempt new list creation from homepage form

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -25,39 +25,61 @@ $(() => {
     function renderListForm() {
         $('#addList-div').children().removeClass('visually-hidden');
     };
-
-// create new group
-router.post('/', async (req, res) => {
     const express = require('express');
-    const { User, Group, GroupUser } = require('../../models');
-
-    try {
-        const { title, memberEmail } = req.body;
-
-        const user = await User.findOne({ where: { id: req.user.id } });
-        const newGroup = await Group.create({ title, description, owning_user_id: user.id }); // Create a new instance of the Group model and associate it with the logged-in user
-
-        const [member, created] = await User.findOrCreate({ where: { name: groupUser } }); // find or create a new instance of the User model based on the given member name. Should change to email instead but unsure if we want to create new user since we don't have a way to invite someone to join by email at this point.
-
-        await newGroup.addUser(member); // associate the new group instance with the member (groupUser)
-
-        res.status(200).send({ message: 'New group created successfully' });
-    } catch (error) {
-        res.status(500).send({ message: 'Failed to create new group' });
-    }
+    const { User, Group } = require('../../models');
+    
+    // create new group
+    router.post('/new-group', async (req, res) => {
+        try {
+            const { title, memberEmail } = req.body;
+            const user = await User.findOne({ where: { id: req.user.id } });
+            const newGroup = await Group.create({ title, owning_user_id: user.id }); // Create a new instance of Group and associate it with logged-in user... should req.session.userId be used instead here?
+            
+            // commenting out below because I think it'd make more sense to add group members from the group page instead.
+            
+            // const [member, created] = await User.findOrCreate({ // Find or create a new User model instance based on the group member's email
+            //     where: { email: memberEmail },
+            //     defaults: { name: memberEmail.split('@')[0], password: temp1234 } // Use the part before the @ symbol as the user's name if a new user is created and set temporary password (as workaround)
+            //   }); // find or create a new instance of the User model based on provided email
+            
+            // await newGroup.addUser(member); // associate the new group instance with the group member/invitee
+            
+            await newGroup.save(); // save new group to db
+            
+            res.status(200).json({ message: 'New group created' });
+        } catch (err) {
+            res.status(500).json({ message: 'Failed to create new group' });
+            res.status(err);
+        }
     });
-// });
 
     // create new list
-    // function createList() {
+    router.post('/new-list', async (req, res) => {
+        const listTitle = req.body; // I think it makes more sense to add items from the list page instead, so that leaves just a list title as the entire body
+        
+        if(!title) {
+            return res.status(400).json({ message: 'Please enter a title for your list.'});
+        };
+
+        try {
+            const user = await User.findByPk(req.user.id); 
+            const newList = await List.create({ title });
+            const listOwner = user.addList(newList);
+
+            res.status(200).json({ message: 'New list saved'});
+        } catch (err) {
+            res.status(500).json({ message: 'Could not create new list'});
+            res.status(err);
+        }
+
+        res.json(newList);
+    });
     // };
     
     $('#new-group-btn').click(renderGroupForm);
     $('#new-list-btn').click(renderListForm);
-
-    $('#new-group-save-btn').click(createGroup);
-    // $('#new-list-save-btn').click(createList);
-
+        
     $('#groups-col').children().eq(3).click(redirectGroup);
     $('#lists-col').children().eq(3).click(redirectList);
+
 });
