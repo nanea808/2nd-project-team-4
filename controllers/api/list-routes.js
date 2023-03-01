1f681beface609d8c0d8e153df79655bd13cfbc7
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-02-28 23:57:41 -0800

    added list title change

--- controllers/api/list-routes.js
+++ controllers/api/list-routes.js
@@ -1,103 +1,114 @@
-const router = require('express').Router();
-const {User, List, Item, Group, GroupList} = require('../../models');
+const router = require("express").Router();
+const { User, List, Item, Group, GroupList } = require("../../models");
 
 // api/lists endpoint
 
-router.get('/', async (req, res) => {
-    try {
-        const listData = await List.findAll({
-            //include: the user the item belongs to, the items in the list, and all groups with the list.
-            include: [
-                {model: User}, 
-                {model: Item}, 
-                {model: Group, through: {model: GroupList}}
-            ]
-        });
-        res.status(200).json(listData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+router.get("/", async (req, res) => {
+  try {
+    const listData = await List.findAll({
+      //include: the user the item belongs to, the items in the list, and all groups with the list.
+      include: [
+        { model: User },
+        { model: Item },
+        { model: Group, through: { model: GroupList } },
+      ],
+    });
+    res.status(200).json(listData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', async (req,res) => {
-    try {
-        const listData = await List.findByPk(req.params.id, {
-            include: [
-                {model: User}, 
-                {model: Item}, 
-                {model: Group, through: {model: GroupList}}
-            ]
-        });
+router.get("/:id", async (req, res) => {
+  try {
+    const listData = await List.findByPk(req.params.id, {
+      include: [
+        { model: User },
+        { model: Item },
+        { model: Group, through: { model: GroupList } },
+      ],
+    });
 
-        if(!listData) {
-            res.status(404).json({message: 'No list with that ID.'});
-            return;
-        }
-        res.status(200).json(listData);
-    } catch (err) {
-        res.status(500).json(err);
+    if (!listData) {
+      res.status(404).json({ message: "No list with that ID." });
+      return;
     }
+    res.status(200).json(listData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //post to create a list
-router.post('/', async (req, res) => {
-    /*req.body should look something like this:
+router.post("/", async (req, res) => {
+  /*req.body should look something like this:
     {
         title: "something",
         user_id: 3,
         groupIds: [1,2,3]
     }
     */
-    List.create(req.body)
-        .then((list) => {
-            if(req.body.groupIds) {
-                const groupListIdArr = req.body.groupIds.map((group_id) => {
-                    return {
-                        list_id: list.id,
-                        group_id
-                    };
-                });
-                GroupList.bulkCreate(groupListIdArr);
-            }
-            return true;
-        })
-        .then((results) => res.status(200).json(results))
-        .catch((err) => {
-          console.log(err);
-          res.status(400).json(err);
+  List.create(req.body)
+    .then((list) => {
+      if (req.body.groupIds) {
+        const groupListIdArr = req.body.groupIds.map((group_id) => {
+          return {
+            list_id: list.id,
+            group_id,
+          };
         });
-  });
-
-  //Remove a group. parameter is list id, group id is in the body.
-  router.put('/:id', async (req, res) => {
-    try {
-        await GroupList.destroy({where: {group_id: req.body.removedGroup, list_id: req.params.id}});
-        if(req.body.title) {
-            const listData = await List.update({title: req.body.title}, {where: {id: req.params.id}});
-            res.status(200).json(listData);
-        }
-        else res.status(200).json({message: `group with ID ${req.body.removedGroup} removed from this list.`});
-      } catch (err) {
-        res.status(500).json(err);
+        GroupList.bulkCreate(groupListIdArr);
       }
-  });
+      return true;
+    })
+    .then((results) => res.status(200).json(results))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+//Alter a group. Can change group title and remove a grouplist if it's included.
+router.put("/:id", async (req, res) => {
+  try {
+    if (req.body.removedGroup) {
+      await GroupList.destroy({
+        where: { group_id: req.body.removedGroup, list_id: req.params.id },
+      });
+    }
+    if (req.body.title) {
+      const listData = await List.update(
+        { title: req.body.title },
+        { where: { id: req.params.id } }
+      );
+      res.status(200).json(listData);
+    } else
+      res
+        .status(200)
+        .json({
+          message: `group with ID ${req.body.removedGroup} removed from this list.`,
+        });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //delete to destroy a list
-router.delete('/:id', async (req, res) => {
-    try {
-      const listData = await List.destroy({
-        where: {id: req.params.id}
-      });
-  
-      if(!listData) {
-        res.status(404).json({message: 'No lists with that id.'});
-        return;
-      }
-  
-      res.status(200).json(listData);
-    } catch (err) {
-      res.status(500).json(err);
+router.delete("/:id", async (req, res) => {
+  try {
+    const listData = await List.destroy({
+      where: { id: req.params.id },
+    });
+
+    if (!listData) {
+      res.status(404).json({ message: "No lists with that id." });
+      return;
     }
-  });
+
+    res.status(200).json(listData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- public/js/listPage.js
+++ public/js/listPage.js
@@ -1,86 +1,111 @@
-(function() {  
+(function () {
+  //create new item
+  async function newItem(event) {
+    event.preventDefault();
 
-    //create new item
-    async function newItem(event){
-      event.preventDefault();
+    const title = $("#item-title").val().trim();
+    const description = $("#item-description").val().trim();
+    const link = $("#item-link").val().trim();
+    const status = "unassigned";
+    const list_id = $(this).data("list_id");
 
-      const title = $('#item-title').val().trim();
-      const description = $("#item-description").val().trim();
-      const link = $("#item-link").val().trim();
-      const status = "unassigned";
-      const list_id = $(this).data('list_id');
-      
-      if (title) {   
-        const response = await fetch("/api/items", {
-          method: "POST",
-          body: JSON.stringify({ title, description, link, status, list_id }),
-          headers: { "Content-Type": "application/json" },
-        });
-  
-        if (response.ok) {
-          location.reload();
-        } else {
-          alert("Failed to add an item.");
-        }
-      }
-    };
-
-    //delete item
-    async function deleteItem(event) {
-      event.preventDefault();
-      const item_id = $(this).data('item_id');
-      const response = await fetch(`/api/items/${item_id}`, {
-        method: "DELETE",
-        headers: {"Content-Type": "application/json"},
+    if (title) {
+      const response = await fetch("/api/items", {
+        method: "POST",
+        body: JSON.stringify({ title, description, link, status, list_id }),
+        headers: { "Content-Type": "application/json" },
       });
 
       if (response.ok) {
         location.reload();
       } else {
-        alert("Failed to delete an item.");
+        alert("Failed to add an item.");
       }
     }
+  }
 
-    //remove from a group
-    async function updateGroups(event) {
-      event.preventDefault();
-      const group_id = $(this).data('group_id');
-      const list_id = $(this).data('list_id');
-      const response = await fetch(`/api/lists/${list_id}`, {
-        method: "PUT",
-        body: `{"removedGroup": "${group_id}"}`,
-        headers: {"Content-Type": "application/json"},
-      });
+  //delete item
+  async function deleteItem(event) {
+    event.preventDefault();
+    const item_id = $(this).data("item_id");
+    const response = await fetch(`/api/items/${item_id}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    });
 
-      if (response.ok) {
-        location.reload();
-      } else {
-        alert("Failed to delete from a group.");
-      }
+    if (response.ok) {
+      location.reload();
+    } else {
+      alert("Failed to delete an item.");
     }
+  }
 
-    //add list to group
-    async function addToGroup(event){
-      event.preventDefault();
+  //remove from a group
+  async function updateGroups(event) {
+    event.preventDefault();
+    const group_id = $(this).data("group_id");
+    const list_id = $(this).data("list_id");
+    const response = await fetch(`/api/lists/${list_id}`, {
+      method: "PUT",
+      body: `{"removedGroup": "${group_id}"}`,
+      headers: { "Content-Type": "application/json" },
+    });
 
-      const group_id = $('#add-group').val();
-      const list_id = $(this).data('list_id');
-      
-        const response = await fetch("/api/groupList", {
-          method: "POST",
-          body: JSON.stringify({ list_id, group_id }),
-          headers: { "Content-Type": "application/json" },
-        });
-  
-        if (response.ok) {
-          location.reload();
-        } else {
-          alert("Failed to add to the selected group.");
-        }
-    };
+    if (response.ok) {
+      location.reload();
+    } else {
+      alert("Failed to delete from a group.");
+    }
+  }
 
-    $("#group-form").submit(addToGroup);
-    $("#item-form").submit(newItem);
-    $("button[id^='item-del-btn']").click(deleteItem);
-    $("button[id^='group-del-btn']").click(updateGroups);
-  })();
+  //add list to group
+  async function addToGroup(event) {
+    event.preventDefault();
+
+    const group_id = $("#add-group").val();
+    const list_id = $(this).data("list_id");
+
+    const response = await fetch("/api/groupList", {
+      method: "POST",
+      body: JSON.stringify({ list_id, group_id }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      location.reload();
+    } else {
+      alert("Failed to add to the selected group.");
+    }
+  }
+
+  //change a list's title
+  async function changeListTitle(event) {
+    event.preventDefault();
+    alert('test');
+    const list_id = $(this).data("list_id");
+    const newTitle = $('#new-list-title').val().trim();
+    const response = await fetch(`/api/lists/${list_id}`, {
+      method: "PUT",
+      body: `{"title": "${newTitle}"}`,
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      location.reload();
+    } else {
+      alert("Failed to change the title of a list.");
+    }
+  }
+
+  // show group and list add forms
+  function showListTitleForm() {
+    $("#listFormDiv").removeClass("visually-hidden");
+  }
+
+  $("#group-form").submit(addToGroup);
+  $("#item-form").submit(newItem);
+  $("#list-title-form").submit(changeListTitle);
+  $("button[id^='item-del-btn']").click(deleteItem);
+  $("button[id^='group-del-btn']").click(updateGroups);
+  $("#list-title-btn").click(showListTitleForm);
+})();
