bc1424b55e1d17bacbe482386ad310f8ae72d7a6
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-03-03 16:33:41 -0800

    fixed owned groups on listpage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -22,13 +22,15 @@ router.get("/", async (req, res) => {
 
     // Get owned groups based on logged in users ID
     const ownedGroupData = await Group.findAll({
-        where: {
-          owning_user_id: req.session.userID,
-        },
+      where: {
+        owning_user_id: req.session.userID,
+      },
     }).catch((err) => {
       res.json(err);
     });
-    const ownedGroups = ownedGroupData.map((group) => group.get({ plain: true }));
+    const ownedGroups = ownedGroupData.map((group) =>
+      group.get({ plain: true })
+    );
 
     // Get lists based on logged in users ID
     const listData = await List.findAll({
@@ -40,7 +42,13 @@ router.get("/", async (req, res) => {
     });
     const lists = listData.map((list) => list.get({ plain: true }));
 
-    res.render("homepage", { groups, lists, ownedGroups, loggedIn: req.session.loggedIn, user_id: req.session.userID });
+    res.render("homepage", {
+      groups,
+      lists,
+      ownedGroups,
+      loggedIn: req.session.loggedIn,
+      user_id: req.session.userID,
+    });
   } else {
     res.render("login");
   }
@@ -64,7 +72,10 @@ router.get("/group/:id", async (req, res) => {
   let user_belongs = false;
 
   for (const user in group.users) {
-    if (group.users[user].id === req.session.userID || group.owning_user_id === req.session.userID) {
+    if (
+      group.users[user].id === req.session.userID ||
+      group.owning_user_id === req.session.userID
+    ) {
       delete group.users[user];
       user_belongs = true;
     }
@@ -104,21 +115,33 @@ router.get("/list/:id", async (req, res) => {
   const groupData = await Group.findAll({
     include: [
       {
-      model: User, through: { model: GroupUser },
-      where: {
-        id: req.session.userID,
-      }},
+        model: User,
+        through: { model: GroupUser },
+        where: {
+          id: req.session.userID,
+        },
+      },
       // {
       //   model: List, through: {model: GroupList},
       //   where: {
       //     list_id: req.params.id
       //   }
       // }
-    ]
+    ],
   }).catch((err) => {
     res.json(err);
   });
 
+  // Get owned groups based on logged in users ID
+  const ownedGroupData = await Group.findAll({
+    where: {
+      owning_user_id: req.session.userID,
+    },
+  }).catch((err) => {
+    res.json(err);
+  });
+
+  const ownedGroups = ownedGroupData.map((group) => group.get({ plain: true }));
   const groups = groupData.map((group) => group.get({ plain: true }));
   const list = listData.get({ plain: true });
 
@@ -126,7 +149,7 @@ router.get("/list/:id", async (req, res) => {
     res.send(`You don't have access to that list.`);
     return;
   } else {
-    res.render("listPage", { list, groups, loggedIn: req.session.loggedIn });
+    res.render("listPage", { list, groups, ownedGroups, loggedIn: req.session.loggedIn });
   }
 });
 
